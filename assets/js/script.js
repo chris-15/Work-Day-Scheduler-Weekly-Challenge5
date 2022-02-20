cfef4cb0c3e25e6a419c33672a0cb0803469e28d
Chris <christopher.sarm15@gmail.com>
2022-02-19 19:49:25 -0500

    added functionality to save button and set it to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
+// declaring variables to later reference in code
 var currentDate= $('#currentDay');
+var saveButton =$('.saveBtn'); 
 
 // display the current date
 currentDate.text(moment().format("dddd, MMMM Do"));
+
+
+// function to save info in textarea when button is clicked
+saveButton.on("click", function() {
+    var time = $(this).siblings(".hour").text();
+    var userText = $(this).siblings(".description").val();
+    console.log(userText, time);
+
+    localStorage.setItem(time, userText);
+
+
+})
+    
+
